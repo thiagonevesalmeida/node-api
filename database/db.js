0178b58ee7d769366f47411470fd27ca492adcb2
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-04-27 11:45:28 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,25 @@
-let users = [{
-    id: 1,
-    name: "Thiago Neves",
-    city: "Rio de Janeiro",
-    avatar: "https://avatars.githubusercontent.com/u/110680813?v=4"
-}]
+let users = [
+    {
+        id: 1,
+        login: "thiagonevesalmeida",
+        location: "Portugal",
+        avatar: "https://avatars.githubusercontent.com/u/110680813?v=4",
+        url: "https://github.com/thiagonevesalmeida"
+    },
+    {
+        id: 2,
+        login: "gustavoguanabara",
+        location: "Brazil",
+        avatar: "https://avatars.githubusercontent.com/u/8683378?v=4",
+        url: "https://github.com/gustavoguanabara"
+    },
+    {
+        id: 3,
+        login: "maykbrito",
+        location: "Brazil",
+        avatar: "https://avatars.githubusercontent.com/u/6643122?v=4",
+        url: "https://github.com/maykbrito"
+    },
+]
 
 module.exports = users
